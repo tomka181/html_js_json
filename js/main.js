d40278fa8d8cb9149e9123a76e7ec0bd49e4533b
tomka <mass@lypke.de>
2019-01-25 23:01:06 +0100

    added content switch

--- js/main.js
+++ js/main.js
@@ -10,53 +10,115 @@ $(".btn_page_switch").on('click', function(event){
     var requestedPage = $(this).val();
     
     // handle AJAX request
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', contentJsonFile);
-    ourRequest.setRequestHeader('Cache-Control', 'no-cache');
-    ourRequest.onload = function() {
-        var contentData = JSON.parse(ourRequest.responseText);
-        var pageData    = contentData[requestedPage];
+    var request = new XMLHttpRequest();
+    request.open('GET', contentJsonFile);
+    request.setRequestHeader('Cache-Control', 'no-cache');
+    request.onload = function() {
+        var contentData = JSON.parse(request.responseText);
+        var jsonPageData    = contentData[requestedPage];
         // render page content
-        renderHTML(pageData, requestedPage);
+        renderPage(jsonPageData, requestedPage);
     };
-    ourRequest.send();
+    request.send();
 
 });
 
 //-----------------------------------------------------------------------------------
 // render page content
-function renderHTML(data, title){
-    console.log(data);
+function renderPage(jsonPageData, title){
+    
+    //-----------------------------------------------------------------------------------
+    // remove all not mentioned section
+    var sections = document.getElementsByTagName("section");
+    var amoungSections = sections.length;
+    
+    for (idxAmountSection = 0; idxAmountSection < amoungSections; idxAmountSection++) {
 
+        // first hide all sections...
+        $(sections[idxAmountSection]).addClass("hide");
+        
+        // figure out if section is mentioned in json file...
+        var sectionId = sections[idxAmountSection].id;
+        for (var jsonSectionElementId in jsonPageData) {
+            try {
+                var n = sectionId.indexOf(jsonSectionElementId);
+                if (n >= 0) {
+                    // revoke hidden section
+                    $(sections[idxAmountSection]).removeClass("hide");
+                }
+            }
+            catch (e){
+                //console.error(e); // ...to fucking tired!
+            }
+        }
+    }
+
+    //-----------------------------------------------------------------------------------
     // change page title
     document.title = title;
 
+    //-----------------------------------------------------------------------------------
     // loop page content object
-    for (var element_id in data) {
-        console.log('--------------------');
-        console.log(element_id);
-
+    for (var sectionElementId in jsonPageData) {
+        
         // get parent block or section by id
-        var parent_element_by_id = document.getElementById(element_id);
-        if (parent_element_by_id != null) {
-            console.log(parent_element_by_id);
+        var jsonSectionData   = jsonPageData[sectionElementId];
+        var sectionElement     = document.getElementById(sectionElementId);
+        
+        if (sectionElement != null) {
+            
+            // loop jsonSectionData
+            for (var elementName in jsonSectionData){
+                var jsonElementValue = jsonSectionData[elementName];
+                
+                // get and set element by tag name 
+                // TODO: check if redundancy is really necessary
+                var elementsByTag = sectionElement.getElementsByTagName(elementName);
+                if (elementsByTag.length != 0) {
 
-            for (var sub_element_name in data[element_id]){
-                console.log(sub_element_name);
+                    if (isArray(jsonElementValue)){
+                        for (var arrayIdx in jsonElementValue){
+                            elementsByTag[arrayIdx].innerHTML = jsonElementValue[arrayIdx];    
+                        }
+                    }
+                    else {
+                        elementsByTag[0].innerHTML = jsonElementValue;
+                    }
+                }
 
-                //getElementsByTagName
+                // get and set element by class name
+                // TODO: check if redundancy is really necessary
+                var elementsByClass = sectionElement.getElementsByClassName(elementName);
+                if (elementsByClass.length != 0) {
+
+                    if (isArray(jsonElementValue)){
+                        
+                        for (var arrayIdx in jsonElementValue){
+                            elementsByClass[arrayIdx].innerHTML = jsonElementValue[arrayIdx];    
+                        }
+                    }
+                    else {
+                        elementsByClass[0].innerHTML = jsonElementValue;
+                    }
+                }
             }
-
         }
-
-        // get target element to change its content
-        //var child_element_by_name = parent_element_by_id.getElementsByTagName('h1');
-        //console.log(child_element_by_name);
-
-        //child_element_by_name.insertAdjacentHTML('beforeend', htmlString);        
-
     }
-
 }
 
 //-----------------------------------------------------------------------------------
+function isArray(jsonObject) {
+    return jsonObject.constructor === Array;
+}
+
+//-----------------------------------------------------------------------------------
+function getElementsStartsWithId( id ) {
+    var children = document.body.getElementsByTagName('*');
+    var elements = [], child;
+    for (var i = 0, length = children.length; i < length; i++) {
+      child = children[i];
+      if (child.id.substr(0, id.length) == id)
+        elements.push(child);
+    }
+    return elements;
+  }
