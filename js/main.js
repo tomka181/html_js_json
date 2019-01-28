61974983a75b9282b686ee55ae3f63d40a24d689
tomka <mass@lypke.de>
2019-01-28 23:50:57 +0100

    changed logic layer

--- js/main.js
+++ js/main.js
@@ -1,51 +1,15 @@
 //-----------------------------------------------------------------------------------
-var contentJsonFile = 'json/content.json';
-var pageContent     = document.getElementById('page-content');
-var btn_page_switch = document.getElementsByClassName('btn_page_switch');
+var contentJsonFile      = 'json/content.json';
 
 //-----------------------------------------------------------------------------------
-// load starting page first
-
-/*var contentData  = JSON.parse('{"startpage" :{ "target-section" : { "snippet": "target-selection" }}}');
-var jsonPageData = contentData["startpage"];
-// render page content
-renderPage(jsonPageData, "Start");
-*/
-
-
-//-----------------------------------------------------------------------------------
-// event on page switch button
-$(".btn_page_switch").on('click', function(event){
-    // get page request
-    console.log('pressed button');
-    var requestedPage = $(this).val();
-    
-    // handle AJAX request
-    var request = new XMLHttpRequest();
-    request.open('GET', contentJsonFile);
-    request.setRequestHeader('Cache-Control', 'no-cache');
-    request.onload = function() {
-        var contentData  = JSON.parse(request.responseText);
-        var jsonPageData = contentData[requestedPage];
-        // render page content
-        renderPage(jsonPageData, requestedPage);
-    };
-    request.send();
-
-});
-
-//-----------------------------------------------------------------------------------
-// render page content
-function renderPage(jsonPageData, title){
-    
-    //-----------------------------------------------------------------------------------
-    // remove all not mentioned section
+// remove all not mentioned section
+function clearPage(jsonPageData) {
     var sections = document.getElementsByTagName("section");
     var amountSections = sections.length;
     
     for (idxSection = 0; idxSection < amountSections; idxSection++) {
 
-        // first hide all sections...
+        // first hide each section...
         $(sections[idxSection]).addClass("hide");
         
         // figure out if section is mentioned in json file...
@@ -63,68 +27,52 @@ function renderPage(jsonPageData, title){
             }
         }
     }
-
-    //-----------------------------------------------------------------------------------
-    // change page title
-    document.title = title;
-
-    //-----------------------------------------------------------------------------------
-    // loop page setting object
-    for (var sectionElementId in jsonPageData) {
-        
-        // get parent block or section by id
-        var sectionSnippet  = jsonPageData[sectionElementId]["snippet"];
-        console.log("sectionSnippet");
-        console.log(sectionElementId);
-        console.log(sectionSnippet);
-
-        $("#" + sectionElementId).load("snippets/" + sectionSnippet + ".txt");
-
-        var jsonSectionData = jsonPageData[sectionElementId]["content"];
-        var sectionElement  = document.getElementById(sectionElementId);
-        
-        if (sectionElement != null) {
-            
-            // loop jsonSectionData
-            for (var elementName in jsonSectionData){
-                var jsonElementValue = jsonSectionData[elementName];
-                
-                // get and set element by tag name 
-                // TODO: check if redundancy is really necessary
-                var elementsByTag = sectionElement.getElementsByTagName(elementName);
-                if (elementsByTag.length != 0) {
-
-                    if (isArray(jsonElementValue)){
-                        for (var arrayIdx in jsonElementValue){
-                            elementsByTag[arrayIdx].innerHTML = jsonElementValue[arrayIdx];    
-                        }
-                    }
-                    else {
-                        elementsByTag[0].innerHTML = jsonElementValue;
-                    }
-                }
-
-                // get and set element by class name
-                // TODO: check if redundancy is really necessary
-                var elementsByClass = sectionElement.getElementsByClassName(elementName);
-                if (elementsByClass.length != 0) {
-
-                    if (isArray(jsonElementValue)){
-                        
-                        for (var arrayIdx in jsonElementValue){
-                            elementsByClass[arrayIdx].innerHTML = jsonElementValue[arrayIdx];    
-                        }
-                    }
-                    else {
-                        elementsByClass[0].innerHTML = jsonElementValue;
-                    }
-                }
-            }
-        }
-    }
 }
 
 //-----------------------------------------------------------------------------------
-function isArray(jsonObject) {
-    return jsonObject.constructor === Array;
+// render content of the requested page
+function renderPage(jsonPageData, requestedPage) {
+
+    //-----------------------------------------------------------------------------------
+    // clear page
+    clearPage(jsonPageData);
+    
+    //-----------------------------------------------------------------------------------
+    // change page title
+    document.title = requestedPage;
+    
+    //-----------------------------------------------------------------------------------
+    // get content by json file 
+    $.each(jsonPageData, function (sectionId, sectionData) {
+        var sectionContent = jsonPageData[sectionId]["content"];
+        if (sectionContent.length != 0){
+            try {
+                document.getElementById(sectionId).innerHTML = getTemplate(sectionId, sectionContent);
+            }
+            catch(err) {
+                $("#section-" + sectionId).addClass("hide");
+            }
+            
+        }
+    });
 }
+
+//-----------------------------------------------------------------------------------
+// switch page content 
+$(".btn_page_switch").on('click', function(event){
+    // get page request
+    var requestedPage = $(this).val();
+    
+    // handle AJAX request
+    var request = new XMLHttpRequest();
+    request.open('GET', contentJsonFile);
+    request.setRequestHeader('Cache-Control', 'no-cache');
+    request.onload = function() {
+        var contentData  = JSON.parse(request.responseText);
+        var jsonPageData = contentData[requestedPage];
+        // render page content
+        renderPage(jsonPageData, requestedPage);
+    };
+    request.send();
+
+});
---
+++ js/snippets.js
@@ -0,0 +1,63 @@
+//-----------------------------------------------------------------------------------
+// templates
+function templateHeader(dataHeader) {return `
+    <h1>${dataHeader['h1']}</h1>
+    <h2>${dataHeader['h2']}</h2>
+`};
+
+function templateInfo(dataHeader) { return`
+    <div class="row">
+    ${dataHeader['info-box'].map(function(infobox){ return `
+    <div class="col-sm-12 col-md-6 mb-box">                
+        <div class="info-box">
+            ${infobox}
+        </div>
+    </div>
+    `}).join('')}
+    </div>
+`};
+
+function templateTeam(dataTeam) { return`
+    <div class="row">
+    ${dataTeam['members'].map(function(member){ return `
+    <div class="col-sm-12 col-md-${12 / dataTeam['members'].length} mb-box">                
+        <div class="info-box">
+            <img src="${member.img}">
+            <h1>${member.name}</h1>
+            <p>${member.function}</p>
+        </div>
+    </div>
+    `}).join('')}
+    </div>
+`};
+
+function templateFooter(dataFooter) {return `
+    <div class="footer-top">
+        ${dataFooter['footer-top']}
+    </div>
+    <div class="footer-bottom">
+        ${dataFooter['footer-bottom']}
+    </div>
+`};
+
+//-----------------------------------------------------------------------------------
+// collection of snippets
+var dictSnippets = {
+    "header"    : templateHeader,
+    "info"      : templateInfo,
+    "team"      : templateTeam,
+    "footer"    : templateFooter
+}
+
+//-----------------------------------------------------------------------------------
+// return a snippet with replaced content
+function getTemplate(key, content){
+    try{
+        console.log(content)
+        var snippet = dictSnippets[key](content);
+        return snippet;
+    }
+    catch (err) {
+        throw false;
+    }
+}
